3a37978de03cd33049cf2b6ef48d2d1cafd20e8f
oshosh <dhtpgus7@gmail.com>
2023-06-04 22:54:00 +0900

    feat: spring api object return json

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,31 @@ public class HelloController {
     model.addAttribute("name", name);
 
     return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  public @ResponseBody String helloString(@RequestParam("name") String name) {
+    return "hello " + name;
+  }
+
+  @GetMapping("hello-api")
+  public @ResponseBody Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
+
+  static class Hello {
+
+    private String name;
+
+    // alt + ins
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
